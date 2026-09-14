1197825a256e6879733351df062f52e3d10e87e8
cheol1203 <cheolwon36@naver.com>
2026-09-14 13:45:00 +0900

    feat(runtime): JobExecution 실행 생명주기 구현

--- src/daesingo/common/__init__.py
+++ src/daesingo/common/__init__.py
@@ -1,1 +1,18 @@
 """공통 runtime 기반의 공개 패키지 경계."""
+
+from .fixtures import CommonFixture, load_common_fixture
+from .job_execution import (
+    InMemoryJobExecutionStore,
+    JobExecution,
+    JobExecutionError,
+    RuntimeContractRef,
+)
+
+__all__ = [
+    "CommonFixture",
+    "InMemoryJobExecutionStore",
+    "JobExecution",
+    "JobExecutionError",
+    "RuntimeContractRef",
+    "load_common_fixture",
+]
